aeef4e9bbcfbb112fe5f18cd759198ddddb6f3db
bindoong01 <mailsky9687@gmail.com>
2026-03-25 16:13:39 +0900

    [FEAT/TM-10] 403 에러코드 및 AccessDeniedHandler 추가

--- src/main/java/back/global/exception/CommonErrorCode.java
+++ src/main/java/back/global/exception/CommonErrorCode.java
@@ -6,6 +6,7 @@ public enum CommonErrorCode implements ErrorCode {
     BAD_REQUEST("400-1", HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
     BAD_REQUEST_STATE("400-2", HttpStatus.BAD_REQUEST, "요청 상태가 올바르지 않습니다."),
     UNAUTHORIZED("401-1", HttpStatus.UNAUTHORIZED, "로그인이 필요합니다."),
+    FORBIDDEN("403-1", HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
     NOT_FOUND("404-1", HttpStatus.NOT_FOUND, "해당 데이터가 존재하지 않습니다."),
     INTERNAL_SERVER_ERROR("500-1", HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
 
