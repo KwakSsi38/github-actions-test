b9dec0dba170a02b87277544a186813d08990eab
kimhss <hyunuary108@kyonggi.ac.kr>
2026-03-26 14:18:52 +0900

    [FEAT/TM-87] AgentData dto 추가

---
+++ src/main/java/back/domain/prompt/dto/AgentData.java
@@ -0,0 +1,27 @@
+package back.domain.prompt.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Getter
+@NoArgsConstructor
+public class AgentData {
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("file_path")
+    private String filePath;
+
+    @JsonProperty("content_md")
+    private String contentMd;
+
+    @JsonProperty("content_hash")
+    private String contentHash;
+
+    @JsonProperty("raw_metadata")
+    private Map<String, Object> rawMetadata;
+}
